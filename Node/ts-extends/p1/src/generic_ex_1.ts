3547fa25deb46c86d1ee27696f3856629219e88b
MatanDori <matand213@gmail.com>
2025-05-07 17:26:00 +0300

    /

---
+++ Node/ts-extends/p1/src/generic_ex_1.ts
@@ -0,0 +1,54 @@
+// Remove UsersApiResponse and AdminsApiResponse types
+// and use generic type ApiResponse in order to specify API
+// response formats for each of the functions.
+
+interface User {
+    type: "user";
+    name: string;
+    age: number;
+    occupation: string;
+}
+
+interface Admin {
+    type: "admin";
+    name: string;
+    age: number;
+    role: string;
+}
+
+type Person = User | Admin;
+
+const admins: Admin[] = [
+    { type: "admin", name: "Jane Doe", age: 32, role: "Administrator" },
+    { type: "admin", name: "Bruce Willis", age: 64, role: "World saver" },
+];
+
+const users: User[] = [
+    {
+        type: "user",
+        name: "Max Mustermann",
+        age: 25,
+        occupation: "Chimney sweep",
+    },
+    { type: "user", name: "Kate Müller", age: 23, occupation: "Astronaut" },
+];
+
+//generic type
+export type ApiResponse<T> = 
+  | { status: "success"; data: T[] }
+  | { status: "error"; error: string };
+
+
+export function requestAdmins(callback: (response: ApiResponse<Admin>) => void) {
+    callback({
+        status: "success",
+        data: admins,
+    });
+}
+
+export function requestUsers(callback: (response: ApiResponse<User>) => void) {
+    callback({
+        status: "success",
+        data: users,
+    });
+}
