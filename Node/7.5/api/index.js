61eb5115b35a1f7bbcf51b714d26577876e40cda
MatanDori <matand213@gmail.com>
2025-05-10 14:55:14 +0300

    /

--- Node/7.5/api/index.js
+++ Node/7.5/api/index.js
@@ -1,75 +1,72 @@
 const express = require("express")
 const cors = require("cors")
-const cors = require("body-parser")
+const bodyParser = require("body-parser")
 
 const config = require("./config")
 console.log(config)
 
-const app = express()//בריאת האפליקציה, מפה הכל מתחיל
+const app = express()
 app.use(cors())
-app.use(bodyParser.json({extends: true}))
-app.use((req, res, next) =>{
-    console.log(req.path)
-    console.log(new Date().toISOString())
-    console.log("I am in middleware")
+app.use(bodyParser.json({ extended: true }))
+app.use((req, res, next) => {
+    console.log(`[${new Date().toISOString()}] Request Started ${req.originalUrl} `)
     next()
 })
 
+// app.use((req, res, next) => {
+//     console.log("I am in a BAD middleware")
+//     return res.send("BAD MIDDLEWARE")
+// })
 
-app.get("/api/health-check" , (req , res , next) =>{
+app.get("/api/health-check", (req, res, next) => {
     console.log(req.query)
     res.send("Server is up and running")
 })
-
-//
-
-app.get("/api/authenticate", (req , res , next) =>{
+// http://localhost:3000/api/authenticate?token=t12345 => 401
+//http://localhost:3000/api/authenticate?token=t1234 => 200 
+app.get("/api/authenticate", (req, res, next) => {
     const token = req?.query?.token
-    if(token === "t1234"){
+    if (token === "t1234") {
         return res.send("User Authorized")
+    } else {
+        return res.status(401).send("Sorry User is not Authorized")
     }
-    else{
-        return res.status(401).send("Sorry , User UnAuthorized")    }
 })
-
 const users = []
-app.get("/auth/register", (req,res,next) =>{
-    const userName = req.query.userName
-    const password = req.query.password
-    if(userName && password){
-        users.push({userName , password})
-        return res.send("User registered succecfully")
-    }
-    else{
-        return res.status(400).send("Missing userName or Password") 
+// TODO: change get to post
+app.post("/auth/register", (req, res, next) => {
+    const userName = req.body.userName
+    const password = req.body.password
+    if (userName && password) {
+        users.push({ userName, password })
+        return res.send("User registered successfully")
+    } else {
+        return res.status(400).send("Missing UserName or Password")
     }
 })
-
-app.get("/auth/login", (req,res,next) =>{
+// TODO: change get to post
+app.get("/auth/login", (req, res, next) => {
     const userName = req.query.userName
     const password = req.query.password
-    if(userName && password){
+    if (userName && password) {
         const foundUser = users.find(u => u.userName === userName && u.password === password)
-        if(foundUser){
-            return res.send("User logged in succecfully")
-        }else{
-            return res.send("Missing userName or Password") 
-        }   
-    }
-    else{
-        return res.status(400).send("Missing userName or Password") 
+        if (foundUser) {
+            return res.send("User Logged in successfully")
+        } else {
+            return res.send("Wrong Username or Password")
+        }
+
+    } else {
+        return res.status(400).send("Missing Username or Password")
     }
 })
-
 app.get("/users", (req, res, next) => {
     res.json(users)
 })
-
-app.listen(config.port , (e) =>{
-    if(e){
+app.listen(config.port, (e) => {
+    if (e) {
         console.log(e)
-    }
-    else{
+    } else {
         console.log(`Listening to PORT: ${config.port}`)
     }
 })
